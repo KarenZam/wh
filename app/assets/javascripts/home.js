f656e110019c8439ddaf96a133fede68c2af763f
chasm <charles.munat@gmail.com>
2014-03-02 23:26:15 +0800

    sob

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,5 @@
+//= require ./vendor/jquery.backstretch.min
+//= require ./vendor/jquery.countdown.min
+//= require ./vendor/packaged/jquery.noty.packaged.min
+//= require ./vendor/themes/default
+//= require ./home/main
